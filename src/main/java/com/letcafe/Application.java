64bb675a268a9359e8afc373afb13977809ed72a
郭冬雨 <letcafe@guodongyudeMacBook-Pro.local>
2019-09-09 10:27:55 +0800

    Initial commit

---
+++ src/main/java/com/letcafe/Application.java
@@ -0,0 +1,25 @@
+package com.letcafe;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.info("Spring application started");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+}
+
---
+++ src/main/java/com/letcafe/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.letcafe.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+    @RequestMapping(path = "hello")
+    public String res() {
+        return "hello world";
+    }
+}
