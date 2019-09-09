411d6462406f1052cc4b18652b5f5730387e0c45
letcafe <letcafe@outlook.com>
2019-09-09 20:23:07 +0800

    test

--- src/main/java/com/letcafe/controller/MyController.java
+++ src/main/java/com/letcafe/controller/MyController.java
@@ -8,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MyController {
 
+    private static int val = 0;
+
     @RequestMapping(path = "hello")
     public String res() {
         return "hello world2";
+    }
+
+    @RequestMapping(path = "hello2")
+    public String res2() {
+        return "hello world" + ++val;
     }
 }
